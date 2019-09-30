66f73cef0f00b2582e0d7531f0a4fa0d16b0a7c8
Xin Jin <jinxin7120@gmail.com>
2019-09-30 16:50:20 +0800

    add infer mode

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,9 @@
 """
 data processing
 """
+import json
 import pandas as pd
 import numpy as np
-
 from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 from common.features import last_year_value, is_workdays, is_holidays
 from scipy import stats
@@ -85,7 +85,24 @@ class DataProcessor:
         else:
             self.num_features = 5
 
-    def process_train_test_data(self, raw, val_ratio=0.1):
+    def _get_x_y(self, df):
+        data = series_to_supervised(df, self.window, self.forecast_length)
+        n_obs = self.window * (self.num_features + 1)
+
+        data_x, data_y = data.iloc[:, :n_obs], data.iloc[:, n_obs:]
+        feature_x = data_x[[col for col in data_x.columns if not col.startswith('y')]]
+        feature_x = feature_x.values.reshape(data_x.shape[0], self.window, self.num_features)
+        label_x = data_x[[col for col in data_x.columns if col.startswith('y')]]
+        label_x = label_x.values.reshape(data_x.shape[0], self.window)
+
+        label_y = data_y[[col for col in data_y.columns if col.startswith('y')]]
+        label_y = label_y.values.reshape(data_x.shape[0], self.forecast_length)
+        feature_y = data_y[[col for col in data_y.columns if not col.startswith('y')]]
+        feature_y = feature_y.values.reshape(data_x.shape[0], self.forecast_length,
+                                             self.num_features)
+        return feature_x, label_x, feature_y, label_y
+
+    def get_train_test_data(self, raw, val_ratio=0.1):
         """
 
         :param raw: pandas dataframe, with column 'date','y','type'
@@ -106,6 +123,7 @@ class DataProcessor:
             raw['type'] = encoder_type
         for type in ts_types:
             df = raw.loc[(raw.type == type)].copy()
+            df = df.sort_values(by='date')
             # features
             df['is_workday'] = is_workdays(df.date)
             df['is_holiday'] = is_holidays(df.date)
@@ -116,21 +134,9 @@ class DataProcessor:
                 del df['type']
             del df['date']
 
-            data = series_to_supervised(df, self.window, self.forecast_length)
-            n_obs = self.window * (self.num_features + 1)
+            feature_x, label_x, feature_y, label_y = self._get_x_y(df)
 
-            data_x, data_y = data.iloc[:, :n_obs], data.iloc[:, n_obs:]
-            feature_x = data_x[[col for col in data_x.columns if not col.startswith('y')]]
-            feature_x = feature_x.values.reshape(data_x.shape[0], self.window, self.num_features)
-            label_x = data_x[[col for col in data_x.columns if col.startswith('y')]]
-            label_x = label_x.values.reshape(data_x.shape[0], self.window)
-
-            label_y = data_y[[col for col in data_y.columns if col.startswith('y')]]
-            label_y = label_y.values.reshape(data_x.shape[0], self.forecast_length)
-            feature_y = data_y[[col for col in data_y.columns if not col.startswith('y')]]
-            feature_y = feature_y.values.reshape(data_x.shape[0], self.forecast_length,
-                                                 self.num_features)
-            val_length = int((data_x.shape[0] - self.forecast_length) * val_ratio)
+            val_length = int((label_x.shape[0] - self.forecast_length) * val_ratio)
             val_start_idx = -(val_length + self.forecast_length)
 
             # scale value as described in deepar paper
@@ -155,8 +161,7 @@ class DataProcessor:
         train_data = list(map(np.concatenate, zip(*train_data_list)))
         train_scale = np.concatenate(train_scale_list)
         train_dataset = TsDataSet(*(train_data + [train_scale]))
-        sampler = WeightedRandomSampler(np.abs(train_scale),
-                                        train_scale.shape[0])
+        sampler = WeightedRandomSampler(np.abs(train_scale), train_scale.shape[0])
         train_loader = DataLoader(train_dataset, batch_size=self.batch_size, sampler=sampler)
 
         val_data = list(map(np.concatenate, zip(*val_data_list)))
@@ -166,11 +171,66 @@ class DataProcessor:
 
         return train_loader, val_loader
 
-    def process_predict_data(self, raw):
-        # TODO: update infer part
+    def get_forecast_data(self, raw):
+        forecast_data_list = []
+        scale_list = []
         raw['date'] = pd.to_datetime(raw['date'])
-        end_date = max(raw.date)
-        future_df = pd.DataFrame({
-            'date': [(end_date + pd.Timedelta(days=i)) for i in range(1, self.forecast_length + 1)],
-            'y': np.zeros(self.forecast_length)})
-        return None
+        ts_types = sorted(raw['type'].unique())
+        if self.use_ts_type:
+            encoder_type = self.label_encoder.transform(raw['type'])
+            del raw['type']
+            raw['type'] = encoder_type
+        for type in ts_types:
+            df = raw.loc[(raw.type == type)].copy()
+            end_date = max(df.date)
+            df = df.sort_values(by='date')
+            future_df = pd.DataFrame({'date': [(end_date + pd.Timedelta(days=i)) for i in
+                                               range(1, self.forecast_length + 1)],
+                                      'y': np.zeros(self.forecast_length),
+                                      'type': [type] * self.forecast_length})
+            df = pd.concat([df, future_df], axis=0, sort=False)
+            # features
+            df['is_workday'] = is_workdays(df.date)
+            df['is_holiday'] = is_holidays(df.date)
+            df['last_year_value'] = stats.zscore(last_year_value(df, 'date', 'y'))
+            df['weekday'] = stats.zscore(df['date'].dt.weekday)
+            df['month'] = stats.zscore(df['date'].dt.month)
+            if not self.use_ts_type:
+                del df['type']
+            del df['date']
+            feature_x, label_x, feature_y, label_y = self._get_x_y(df)
+            # scale value as described in deepar paper
+            forecast_feature_x = feature_x[-1:, :, :]
+            forecast_label_x = label_x[-1:, :]
+            forecast_feature_y = feature_y[-1:, :, :]
+            # it's fake data, just for using the Dataset
+            forecast_label_y = label_y[-1:, :]
+            assert np.sum(forecast_label_y) == 0
+
+            scale = 1 + np.mean(forecast_label_x, axis=1)
+            forecast_label_x = forecast_label_x / scale[:, np.newaxis]
+            forecast_data_list.append(
+                [forecast_label_x, forecast_feature_x, forecast_label_y, forecast_feature_y])
+            scale_list.append(scale)
+        data = list(map(np.concatenate, zip(*forecast_data_list)))
+        scale = np.concatenate(scale_list)
+        dataset = TsDataSet(*(data + [scale]))
+        loader = DataLoader(dataset, batch_size=1)
+        return loader, ts_types
+
+    def save(self, file_name):
+
+        if self.use_ts_type:
+            np.save(file_name + '/classes.npy', self.label_encoder.classes_)
+        with open(file_name + '/data_process_params.json', 'w') as f:
+            json.dump({'forecast_length': self.forecast_length, 'batch_size': self.batch_size,
+                       'window': self.window, 'use_ts_type': self.use_ts_type}, f)
+
+    @staticmethod
+    def load(file_name):
+        with open(file_name + '/data_process_params.json', 'r') as f:
+            param = json.load(f)
+        data_processor = DataProcessor(**param)
+        if data_processor.use_ts_type:
+            data_processor.label_encoder.classes_ = np.load(file_name + '/classes.npy')
+        return data_processor
--- lstm/train.py
+++ lstm/train.py
@@ -14,6 +14,7 @@ def cli_flag_argparser():
     """"Parser for command line flags."""
     flag_parser = argparse.ArgumentParser()
     flag_parser.add_argument('--data_path', type=str, default='../data/data.csv')
+    flag_parser.add_argument('--mode', type=str, default='train')
     flag_parser.add_argument('--checkpoint_path', type=str, default='')
     flag_parser.add_argument('--num_units', type=int, default=128)
     flag_parser.add_argument('--output_dim', type=int, default=1)
@@ -30,7 +31,7 @@ def cli_flag_argparser():
     return flag_parser
 
 
-def load_checkpoint(model, optimizer, file_name):
+def load_checkpoint(file_name, model, optimizer=None):
     """
     load model and optimizer
     :param model:
@@ -39,13 +40,14 @@ def load_checkpoint(model, optimizer, file_name):
     :return:
     """
     start_epoch = 0
-    if os.path.isfile(file_name):
+    if os.path.isfile(file_name + '/model.pt'):
         print("=> loading checkpoint '{}'".format(file_name))
-        checkpoint = torch.load(file_name)
+        checkpoint = torch.load(file_name + '/model.pt')
         start_epoch = checkpoint['epoch']
         model.load_state_dict(checkpoint['state_dict'])
         model.eval()
-        optimizer.load_state_dict(checkpoint['optimizer'])
+        if optimizer:
+            optimizer.load_state_dict(checkpoint['optimizer'])
         # if use gpu should manually move to gpu
         print("=> loaded checkpoint '{}' (epoch {})".format(file_name, start_epoch))
     else:
@@ -54,8 +56,9 @@ def load_checkpoint(model, optimizer, file_name):
     return model, optimizer, start_epoch
 
 
-def _train(data, model, loss_fn, window, forecast_length, teacher_ratio):
-    label_x, feature_x, label_y, feature_y, scale = data
+def _forward(data, model, loss_fn, window, forecast_length, teacher_ratio):
+    outputs = []
+    label_x, feature_x, label_y, feature_y, _ = data
     new_batch_size = label_x.shape[0]
     model.batch_size = new_batch_size
     model.hidden = model.init_hidden(new_batch_size)
@@ -72,23 +75,24 @@ def _train(data, model, loss_fn, window, forecast_length, teacher_ratio):
             inp = torch.cat([output.reshape([-1, 1, 1]), feature_y[:, idx:idx + 1, :]], dim=2)
 
         output = model(inp)
+        outputs.append(output)
         loss += loss_fn(output, y_true)
     avg_loss = loss / forecast_length
-    return avg_loss
+    return avg_loss, outputs
 
 
 def evaluate(val_loader, model, loss_fn, window, forecast_length):
     losses = []
     with torch.no_grad():
         for step, data in enumerate(val_loader):
-            loss = _train(data, model, loss_fn, window, forecast_length, 1.)
+            loss, _ = _forward(data, model, loss_fn, window, forecast_length, 1.)
             losses.append(loss)
     return np.mean(losses)
 
 
 def train(raw, flags):
     data_processor = DataProcessor(flags.forecast_length, flags.batch_size, flags.window, False)
-    train_loader, val_loader = data_processor.process_train_test_data(raw, flags.validation_ratio)
+    train_loader, val_loader = data_processor.get_train_test_data(raw, flags.validation_ratio)
 
     model = LSTM(data_processor.num_features + 1, flags.num_units, output_dim=flags.output_dim,
                  num_layers=flags.num_layers, batch_first=True, dropout=flags.dropout)
@@ -100,15 +104,15 @@ def train(raw, flags):
     teacher_ratio = flags.teacher_ratio
     loss_history = []
 
-    model, opt, start_epoch = load_checkpoint(model, opt, flags.checkpoint_path)
+    model, opt, start_epoch = load_checkpoint(flags.checkpoint_path, model, opt)
     if start_epoch >= flags.num_epochs:
         print('start_epoch is larger than num_epochs!')
     epoch = start_epoch
     # TODO: add early stop
     for epoch in range(start_epoch, flags.num_epochs):
         for step, data in enumerate(train_loader):
-            avg_loss = _train(data, model, loss_fn, flags.window, flags.forecast_length,
-                              teacher_ratio)
+            avg_loss, _ = _forward(data, model, loss_fn, flags.window, flags.forecast_length,
+                                   teacher_ratio)
             loss_history.append(avg_loss)
             opt.zero_grad()
             avg_loss.backward()
@@ -122,55 +126,48 @@ def train(raw, flags):
         print()
 
     print('Model training completed and save at %s' % flags.checkpoint_path)
-
     state = {'epoch': epoch + 1, 'state_dict': model.state_dict(), 'optimizer': opt.state_dict()}
-    torch.save(state, flags.checkpoint_path)
+    if not os.path.exists(flags.checkpoint_path):
+        os.mkdir(flags.checkpoint_path)
+    torch.save(state, flags.checkpoint_path + '/model.pt')
+    data_processor.save(flags.checkpoint_path)
     return model, loss_history
 
 
 # pylint: disable=too-many-locals
-def infer(hparam, train_model, forecast_data, num_features, norm_list):
+def infer(raw, flags):
     """
-    infer
-    :param hparam:
-    :param train_model:
-    :param forecast_data:
-    :param num_features:
-    :param norm_list:
+
+    :param raw:
+    :param flags:
     :return:
     """
-    # TODO: update infer
-    model = LSTM(num_features, hparam.num_units, output_dim=hparam.output_dim,
-                 num_layers=hparam.num_layers, batch_first=True, dropout=0)
-    model.load_state_dict(train_model.state_dict())
+    data_processor = DataProcessor.load(flags.checkpoint_path)
+    model = LSTM(data_processor.num_features + 1, flags.num_units, output_dim=flags.output_dim,
+                 num_layers=flags.num_layers, batch_first=True, dropout=flags.dropout)
+    loss_fn = SMAPE()
+    model, _, _ = load_checkpoint(flags.checkpoint_path, model, None)
     model.eval()
     # predict
-    results = []
-    for index, data in enumerate(forecast_data):
-        result = []
-        predictions = []
-        train_x, feature_y = map(torch.tensor, data)
-        new_batch_size = train_x.shape[0]
-        model.batch_size = new_batch_size
-        mean, std = norm_list[index]
-        model.hidden = model.init_hidden(new_batch_size)
-        for idx in range(hparam.forecast_length):
-            if idx == 0:
-                inp = train_x
-            else:
-                inp = torch.cat([predictions[-1].reshape([-1, 1, 1]), feature_y[:, idx - 1:idx, :]],
-                                dim=2)
-            y_pred = model(inp)
-            predictions.append(y_pred)
-            pred = y_pred.detach().numpy() * std + mean
-            result.append(pred[0])
-        results.append(result)
+    results = {}
+    loader, ts_types = data_processor.get_forecast_data(raw)
+    with torch.no_grad():
+        for type, data in zip(ts_types, loader):
+            scale = data[4]
+            _, outputs = _forward(data, model, loss_fn, flags.window, flags.forecast_length, 1.)
+            results[type] = [(output * scale).detach().numpy()[0] for output in outputs]
+    print(results)
     return results
 
 
 def main(flags):
     raw = pd.read_csv(flags.data_path)
-    train(raw, flags)
+    if flags.mode == 'train':
+        train(raw, flags)
+    elif flags.mode == 'infer':
+        infer(raw, flags)
+    else:
+        raise ValueError('argument --mode must be train or infer')
 
 
 if __name__ == '__main__':
